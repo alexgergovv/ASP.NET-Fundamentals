7932809be0462beaad5ce2f0e1828671c47fcdf7
Alex Gergov <alexgergovv@gmail.com>
2024-01-26 11:54:31 +0200

    Add files via upload

---
+++ ChatApp/ChatApp/Models/MessageViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ChatApp.Models
+{
+	public class MessageViewModel
+	{
+		/// <summary>
+		/// Message Sender
+		/// </summary>
+		public string Sender { get; set; } = null!;
+		/// <summary>
+		/// Text Message
+		/// </summary>
+		public string MessageText { get; set; } = null!;
+    }
+}
